c2f9a99df79b2a3d0b5b0127c4bac3f74ab1464d
Justin John <34035011+justinjohn0306@users.noreply.github.com>
2025-01-27 13:06:37 +0530

    Update tacotron2.py

--- nemo/collections/tts/models/tacotron2.py
+++ nemo/collections/tts/models/tacotron2.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional
 
 import torch
